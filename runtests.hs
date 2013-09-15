4c5b0cac9d6f58ca4bb01f4893b594dac313262c
vagrant <vagrant@precise32.(none)>
2013-09-15 02:20:42 +0000

    Add test for short open tag

--- runtests.hs
+++ runtests.hs
@@ -60,6 +60,7 @@ testSuite = testGroup "Parser"
             , testCase "Echo statement" (testFile "tests/echo_statement.php" (show $ [PHPCode $ Seq [Echo [Literal $ PHPInt 1, Literal $ PHPInt 2]]]))
             , testCase "Unary prefix ops" (testFile "tests/unary_prefix_ops.php" (show $ [PHPCode (Seq [Expression (UnaryExpr Before Increment (PHPVariable "bar")),Expression (UnaryExpr Before Decrement (PHPVariable "foo"))])]))
             , testCase "Unary postfix ops" (testFile "tests/unary_postfix_ops.php" (show $ [PHPCode (Seq [Expression (UnaryExpr After Increment (PHPVariable "bar")),Expression (UnaryExpr After Decrement (PHPVariable "foo"))])]))
+            , testCase "Short PHP open tag" (testFile "tests/short_open_tag.php" (show $ [PHPCode (Seq [Echo [Literal (PHPString "test")]])]))
             ]
 
 testFile :: FilePath -> String -> IO ()
